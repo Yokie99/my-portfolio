4106a3fa8d4acd112d7bc45cd970e6207e632f84
Yokie99 <kyleym@uci.edu>
2024-05-06 11:50:00 -0700

    saving progress

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,12 +4,12 @@ import headshot from "../assets/Kyle_Ma_02_CSAS6_Headshots.jpg"
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id='about' className=' min-h-screen'>
     <div className="grid grid-cols-2 mx-20 mt-20 px-12 py-4">
       <div className="p-5">
         <h1 className="text-7xl text-white py-3">Kyle, </h1>
-      <h2 className="text-5xl py-3"> Junior Web Developer</h2>
-      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi laborum quaerat vero quisquam, nulla enim soluta harum facilis repellat perspiciatis at provident maiores? Voluptatem quo ab inventore, voluptatibus dolore enim? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam necessitatibus sint eos dolorum cumque, sapiente enim delectus optio nihil est, atque perspiciatis ducimus, porro at? Aliquid illum inventore eveniet ea.</p>
+      <h2 className="text-5xl py-3"> Junior FullStack Developer</h2>
+      <p> Hello, my name is Kyle Yok Eyh Ma. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi laborum quaerat vero quisquam, nulla enim soluta harum facilis repellat perspiciatis at provident maiores? Voluptatem quo ab inventore, voluptatibus dolore enim? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam necessitatibus sint eos dolorum cumque, sapiente enim delectus optio nihil est, atque perspiciatis ducimus, porro at? Aliquid illum inventore eveniet ea.</p>
       </div>
 
       <div className="flex justify-center">
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,17 +1,55 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import Image from "next/image";
+import pokemonSS from "../assets/pokemon_Screencap.png"
 
 const Projects = () => {
-  return (
-    <section id='projects' className='py-6'>
 
-    <div className='flex justify-center'>
-        <h1 className='text-5xl'>Projects</h1>
-    </div>
+    const [isSelected, setIsSelected] = useState<Boolean>(false)
+    const buttonStyles = isSelected
+        ? "text-white border-primary-500"
+        : "text-[#ADB7BE] border-slate-600 hover:border-white";
 
-    
+    return (
+        <section id='projects' className='py-20 min-h-screen'>
 
-    </section>
-  )
+            <div className='flex justify-center'>
+                <h1 className='text-5xl'>Projects</h1>
+
+            </div>
+            <div className="flex flex-row justify-center items-center gap-2 py-6">
+                <button className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}>
+                    PokeDex Project
+                </button>
+                <button className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}>
+                    Project 1
+                </button>
+                <button className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}>
+                    Project 1
+                </button>
+            </div>
+
+
+            <div className='relative '>
+                <div className='w-[60vw] transition-transform duration-300 transform-gpu hover:scale-90 bg-blue-600 pe-4 pt-4 pb-4'>
+                    <a href="https://pokemonreactrebuild.vercel.app/" target="_blank" rel="noopener noreferrer">
+                    <Image className='cursor-pointer ' src={pokemonSS} alt={'preview of pokemon react project'} />
+                    </a>
+                </div>
+                <div className='projDes p-16 absolute top-20 right-10 flex flex-col min-w-[40vw] max-w-[40vw]'>
+                    <div>
+                        <h1 className='text-4xl py-3'>PokeDex</h1>
+                        <h2>React project utilizing the Pokemon API to call fetch its data. Local storge used to save favorites.</h2>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+
+        </section>
+    )
 }
 
 export default Projects
