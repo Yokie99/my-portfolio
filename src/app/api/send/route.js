b0881c35d5b9d280f7f9ed699c629c1c9398d657
Yokie99 <kyleym@uci.edu>
2024-05-07 02:21:05 -0700

    fixed the email issue

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -9,7 +9,7 @@ export async function POST(req, res) {
   console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: fromEmail,
+      from: 'Acme <onboarding@resend.dev>',
       to: [fromEmail, email],
       subject: subject,
       react: (
@@ -26,3 +26,24 @@ export async function POST(req, res) {
     return NextResponse.json({ error });
   }
 }
+
+// import { EmailTemplate } from '../../../app/components/EmailTemplate';
+// import { Resend } from 'resend';
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export async function POST() {
+//   try {
+//     const data = await resend.emails.send({
+//       from: 'Acme <onboarding@resend.dev>',
+//       to: ['delivered@resend.dev'],
+//       subject: 'Hello world',
+//       react: EmailTemplate({ firstName: 'John' }),
+//     });
+
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error });
+//   }
+// }
+
