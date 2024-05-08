d88031146580c1fcb8de813733ed6fdd0d15c8f5
Yokie99 <kyleym@uci.edu>
2024-05-08 15:49:09 -0700

    eod updates

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -5,7 +5,7 @@ import headshot from "../assets/Kyle_Ma_02_CSAS6_Headshots.jpg";
 const About = () => {
   return (
     <section id="about" className=" min-h-screen">
-      <div className="grid grid-cols-2 mx-20 mt-20 px-8 pb-4 py-20">
+      <div className="grid grid-cols-2 mx-24 mt-20 pb-4 py-20">
         <div className="p-5">
           <h1 className="text-7xl text-white py-3">Kyle, </h1>
           <h2 className="text-5xl py-3"> Junior FullStack Developer</h2>
@@ -20,61 +20,66 @@ const About = () => {
             inventore eveniet ea.
           </p>
 
-          <div className="mt-24">
-          <h1>Skills:</h1>
-          <div className="grid grid-cols-4 gap-3">
-            <div className="icons">
-              <h1>Languages:</h1>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+          <div className="mt-24 text-xl">
+            <h1>Skills:</h1>
+            <div className="grid grid-cols-2 gap-3">
+              <div className="icons py-5">
+                <h1>Languages:</h1>
+                <div className="flex flex-row">
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
 
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg" />
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" />
 
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg" />
+                </div>
+              </div>
+
+              <div className="icons py-5">
+                <h1>Productivity</h1>
+                <div className="flex flex-row">
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/slack/slack-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azure/azure-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original-wordmark.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" />
+                </div>
+                
+              </div>
+
+              <div className="icons py-5">
+                <h1>Frameworks</h1>
+                <div className="flex flex-row">
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original-wordmark.svg" />
+
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-plain.svg" />
+                </div>
+                
+              </div>
+
+              <div className="icons py-5">
+                <h1>Libraries</h1>
+                <div>
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+                </div>
+                
+              </div>
             </div>
-
-            <div className="icons">
-              <h1>Productivity</h1>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/slack/slack-original.svg" />
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azure/azure-original.svg" />
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original-wordmark.svg" />
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" />
-            </div>
-
-            <div className="icons">
-              <h1>Frameworks</h1>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original-wordmark.svg" />
-
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-plain.svg" />
-        
-            </div>
-
-            <div className="icons">
-              <h1>Libraries</h1>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
-            </div>
-
-            
           </div>
         </div>
-        </div>
 
-        
-
-        <div className="flex justify-center">
+        <div className="flex justify-center items-start">
           <Image
             className=" rounded-full"
             src={headshot}
             alt={"Kyle Ma Web Dev"}
             height={600}
+            
           />
         </div>
-
-        
       </div>
     </section>
   );
--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,67 +1,105 @@
-'use client'
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
-import pokemonSS from "../assets/pokemon_Screencap.png"
+import pokemonSS from "../assets/pokemon_Screencap.png";
 
 const Projects = () => {
+  const [isSelected1, setIsSelected1] = useState(false);
+  const [isSelected2, setIsSelected2] = useState(false);
+  const [isSelected3, setIsSelected3] = useState(false);
+  const [btnNumber, setBtnNumber] = useState(0);
 
-    const [isSelected, setIsSelected] = useState(false)
-    const buttonStyles = isSelected
-        ? "text-white border-primary-500"
-        : "text-[#ADB7BE] border-slate-600 hover:border-white";
+  const buttonStyles1 = isSelected1
+    ? "text-white border-primary-500"
+    : "text-blue-500 border-slate-600 hover:border-white";
+  const buttonStyles2 = isSelected2
+    ? "text-white border-primary-500"
+    : "text-blue-500 border-slate-600 hover:border-white";
+  const buttonStyles3 = isSelected3
+    ? "text-white border-primary-500"
+    : "text-blue-500 border-slate-600 hover:border-white";
 
-    return (
-        <section id='projects' className='py-20 min-h-screen'>
+  const handleBtnClick = (arg) => {
+    switch (arg) {
+      case 1:
+        setIsSelected1(true);
+        setIsSelected2(false);
+        setIsSelected3(false);
+        break;
+      case 2:
+        setIsSelected1(false);
+        setIsSelected2(true);
+        setIsSelected3(false);
+        break;
+      case 3:
+        setIsSelected1(false);
+        setIsSelected2(false);
+        setIsSelected3(true);
+        break;
+      default:
+        break;
+    }
+  };
+  return (
+    <section id="projects" className="py-20 min-h-screen">
+      <div className="flex justify-center">
+        <h1 className="text-5xl">Projects</h1>
+      </div>
+      <div className="flex flex-row justify-center items-center gap-2 py-6 ">
+        <button
+          className={`${buttonStyles1} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+          onClick={() => handleBtnClick(1)}
+        >
+          PokeDex Project
+        </button>
+        <button
+          className={`${buttonStyles2} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+          onClick={() => handleBtnClick(2)}
+        >
+          Project 1
+        </button>
+        <button
+          className={`${buttonStyles3} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+          onClick={() => handleBtnClick(3)}
+        >
+          Project 1
+        </button>
+      </div>
 
-            <div className='flex justify-center'>
-                <h1 className='text-5xl'>Projects</h1>
-
-            </div>
-            <div className="flex flex-row justify-center items-center gap-2 py-6">
-                <button className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}>
-                    PokeDex Project
-                </button>
-                <button className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}>
-                    Project 1
-                </button>
-                <button className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}>
-                    Project 1
-                </button>
-            </div>
-
-
-            <div className='relative '>
-                <div className='w-[60vw] transition-transform duration-300 transform-gpu hover:scale-90 bg-blue-600 pe-4 pt-4 pb-4'>
-                    <a href="https://pokemonreactrebuild.vercel.app/" target="_blank" rel="noopener noreferrer">
-                    <Image className='cursor-pointer ' src={pokemonSS} alt={'preview of pokemon react project'} />
-                    </a>
-                </div>
-                <div className='projDes p-16 absolute top-20 right-10 flex flex-col min-w-[40vw] max-w-[40vw]'>
-                    <div>
-                        <h1 className='text-4xl py-3'>PokeDex</h1>
-                        <h2>React project utilizing the Pokemon API to call fetch its data. Local storge used to save favorites.</h2>
-                        <p>Built with:</p>
-                        <div className='grid grid-cols-4 gap-3'>
-        
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
-          
-      
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
-      
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
-      
-          
+      <div className="grid grid-cols-7">
+        <div className="col-span-5 transition-transform duration-300 transform-gpu hover:scale-90 bg-blue-600 pe-4 pt-4 pb-4">
+          <a
+            href="https://pokemonreactrebuild.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="cursor-pointer "
+              src={pokemonSS}
+              alt={"preview of pokemon react project"}
+            />
+          </a>
         </div>
-                    </div>
+        <div className="projDes p-3 top-20 right-10 flex  items-center col-span-2">
+          <div>
+            <h1 className="text-4xl py-3">PokeDex</h1>
+            <h2>
+              React project utilizing the Pokemon API to call fetch its data.
+              Local storge used to save favorites.
+            </h2>
+            <p>Built with:</p>
+            <div className="grid grid-cols-4 gap-3">
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
 
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
 
-                </div>
-
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
             </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-
-        </section>
-    )
-}
-
-export default Projects
+export default Projects;
