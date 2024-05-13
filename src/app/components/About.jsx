a151fc86e65285bee7c6b6353bc8b53534790494
Yokie99 <kyleym@uci.edu>
2024-05-12 19:03:26 -0700

    new icon layout

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -5,11 +5,11 @@ import headshot from "../assets/Kyle_Ma_02_CSAS6_Headshots.jpg";
 const About = () => {
   return (
     <section id="about" className=" min-h-screen">
-      <div className="grid grid-cols-2 mx-24 mt-20 pb-4 py-20">
-        <div className="p-5">
+      <div className="grid lg:grid-cols-2 mx-6 lg:mx-24 mt-24 ">
+        <div className="">
           <h1 className="text-7xl text-white py-3">Kyle, </h1>
           <h2 className="text-5xl py-3"> Junior FullStack Developer</h2>
-          <p>
+          <p className="text-xl">
             Hello, my name is Kyle Yok Eyh Ma. Lorem ipsum dolor sit amet
             consectetur adipisicing elit. Nisi laborum quaerat vero quisquam,
             nulla enim soluta harum facilis repellat perspiciatis at provident
@@ -19,56 +19,6 @@ const About = () => {
             nihil est, atque perspiciatis ducimus, porro at? Aliquid illum
             inventore eveniet ea.
           </p>
-
-          <div className="mt-24 text-xl">
-            <h1>Skills:</h1>
-            <div className="grid grid-cols-2 gap-3">
-              <div className="icons py-5">
-                <h1>Languages:</h1>
-                <div className="flex flex-row">
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
-
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" />
-
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg" />
-                </div>
-              </div>
-
-              <div className="icons py-5">
-                <h1>Productivity</h1>
-                <div className="flex flex-row">
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/slack/slack-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azure/azure-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original-wordmark.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" />
-                </div>
-                
-              </div>
-
-              <div className="icons py-5">
-                <h1>Frameworks</h1>
-                <div className="flex flex-row">
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original-wordmark.svg" />
-
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-plain.svg" />
-                </div>
-                
-              </div>
-
-              <div className="icons py-5">
-                <h1>Libraries</h1>
-                <div>
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
-                </div>
-                
-              </div>
-            </div>
-          </div>
         </div>
 
         <div className="flex justify-center items-start">
@@ -76,9 +26,55 @@ const About = () => {
             className=" rounded-full"
             src={headshot}
             alt={"Kyle Ma Web Dev"}
-            height={600}
-            
+            height={380}
           />
+        </div>
+      </div>
+
+      <div className="text-xl pb-4 py-16 ">
+        <h1 className=" text-center py-6">Skills:</h1>
+        <div className="flex flex-row gap-7 justify-center">
+          
+            <div>
+              <h1>Libraries:</h1>
+
+            <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+            </div>
+
+            <div>
+              <h1>Languages:</h1>
+            <div className="flex flex-row gap-2">
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg" />
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" />
+
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg" /></div>
+            </div>
+          
+          <div className="">
+            <h1>Frameworks</h1>
+            <div className="flex flex-row gap-2">
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original-wordmark.svg" />
+
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-plain.svg" />
+            </div>
+          </div>
+          <div className="">
+            <h1>Productivity</h1>
+            <div className="flex flex-row gap-2">
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/slack/slack-original.svg" />
+              <img className="icons"src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azure/azure-original.svg" />
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original-wordmark.svg" />
+              <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" />
+            </div>
+          </div>
+
+
         </div>
       </div>
     </section>
